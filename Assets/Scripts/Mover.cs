99904ac64b7ff873d298675473219b542f6a5e4d
shawhien <shawhien@gmail.com>
2020-04-03 15:32:09 -0700

    add left click input

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Raycast from camera to left mouse-click location
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
     }
+  
 }
-
