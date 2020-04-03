ea529a4601009bfae21df1b85c4087196a213e81
shawhien <shawhien@gmail.com>
2020-04-03 15:40:52 -0700

    move to target location on left click

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -16,5 +16,19 @@ public class Mover : MonoBehaviour
             MoveToCursor();
         }
     }
-  
+    //implement click to move on left click
+    private void MoveToCursor()
+    {
+        //create variable parameters for Raycast()
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // https://docs.unity3d.com/ScriptReference/RaycastHit.html
+        RaycastHit hit;
+        //when hasHit is true, store position of raycast in (out) hit.
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            //https://docs.unity3d.com/ScriptReference/RaycastHit-point.html
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
 }
