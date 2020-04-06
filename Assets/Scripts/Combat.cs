20a5ea089a546d81ee2b8aec3a87624aae27d6f1
shawhien <shawhien@gmail.com>
2020-04-05 19:17:30 -0700

    better print statement for entertainment since i need a break from figuring out this bug

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -4,6 +4,6 @@ public class Combat : MonoBehaviour
 {
     public void Attack(AttackTarget target)
     {
-        print("We wear pink on Wednesdays. DIE!");
+        print("Your mother was a hamster and your father smelt of elderberries.");
     }
 }
