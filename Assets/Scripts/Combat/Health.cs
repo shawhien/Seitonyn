24f27574bfb79b40f6801d5f16612a71a3ef48bb
shawhien <shawhien@gmail.com>
2020-04-08 14:56:41 -0700

    Health variable set

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 namespace RPG.combat
 {
-
-public class Health : MonoBehaviour
-{
+    public class Health: MonoBehaviour
+    {
         //set initial health to 100 float.
-       [SerializedField] float health = 100f;
+        [SerializeField] float health = 100f;
 
+        public void TakeDamage(float damage)
+        {
+            //Health cannot go below 0
+            //https://forum.unity.com/threads/how-to-make-an-integer-not-go-below-0.149183/
+            health = Mathf.Max(health - damage, 0);
+            print(health);
+        }
+    }
 }
+
 
