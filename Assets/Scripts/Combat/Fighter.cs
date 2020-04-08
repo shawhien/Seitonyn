0f75893c06bb57be90fc87012080ae153b817d78
shawhien <shawhien@gmail.com>
2020-04-08 14:53:30 -0700

    takeDamage function implemented

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,24 +7,23 @@ namespace RPG.Combat
     {
         //Set range of character to 2m from target within Unity
         //https://answers.unity.com/questions/282128/what-does-0f-and-5f-mean.html
-        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float range = 2f;
 
         Transform target;
 
         //Update to be able to move to/attack existing targets
         public void Update()
         {
+            //Check the distance between player position
+            bool isInRange = Vector3.Distance(transform.position, target.position) < range;
            //If target exists, move to that target
-           if (target == null)
+           if (target != null && !isInRange)
             {
-                return;
-            }
-           if (!GetInRange())
-           {
                 GetComponent<Mover>().MoveTo(target.position);
-           }
+            }
             else
             {
+                GetComponent<Mover>().Stop();
                 GetComponent<Animator>().SetTrigger("attack");
 
             }
@@ -34,7 +33,7 @@ namespace RPG.Combat
         private bool GetInRange()
         {
             //https://answers.unity.com/questions/1288414/move-player-to-exact-vector3-coordinates-beginner.html
-            return Vector3.Distance(transform.position, target.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.position) < range;
         }
 
         public void Attack(CombatTarget combattarget)
@@ -44,6 +43,13 @@ namespace RPG.Combat
             //Set enemy to combat target
             target = combattarget.transform;
         }
+
+        ////Stop attacking when click to move (WiP)
+        ////https://stackoverflow.com/questions/38057082/attack-timer-stops-after-killing-enemy-when-attack-script-loose-target-unity
+        //public void Cancel()
+        //{
+        //    target = null;
+        //}
 
         //Animation for hitting
         public void Hit()
---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.combat
+{
+
+public class Health : MonoBehaviour
+{
+        //set initial health to 100 float.
+       [SerializedField] float health = 100f;
+
+}
+
