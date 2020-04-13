68dc374b381ad396264c8521a5f4c2139e2b4029
shawhien <shawhien@gmail.com>
2020-04-12 22:42:49 -0700

    Death animations and environment updates

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -6,15 +6,33 @@ namespace RPG.Combat
     public class Health: MonoBehaviour
     {
         //set initial health to 100 float.
-        [SerializeField] float health = 20f;
+        [SerializeField] float healthPoints = 20f;
+
+        bool isDead = false;
 
         public void TakeDamage(float damage)
         {
             //Health cannot go below 0
             //https://forum.unity.com/threads/how-to-make-an-integer-not-go-below-0.149183/
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+            print(healthPoints);
         }
+
+        private void Die()
+        {
+            if (isDead)
+            {
+                return;
+            }
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+        }
+           
     }
 }
 
